07d5f8a0fab91c6acf85ed0beddad856fce09d97
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2017-11-24 21:09:28 -0800

    Render posts very simply

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
+let postsUrl = 'https://raw.githubusercontent.com/babelthuap/hayalab-blog/master/posts.json';
+
 $(document).ready(function() {
-  console.log('jQuery is ready');
+
+  let postsDiv = $('#posts');
+
+  $.get(postsUrl)
+      .done(function(data) {
+        console.log('data:', data);
+        let posts = JSON.parse(data);
+        for (let post of posts) {
+          let $post = $('<div>');
+          
+          let $title = $('<h3>');
+          $title.text(post.title);
+          $post.append($title);
+
+          let $content = $('<p>');
+          $content.text(post.content);
+          $post.append($content);
+
+          postsDiv.append($post);
+        }
+      })
+      .fail(function(err) {
+        console.log('There was an error fetching posts:', err);
+      });
+
 });
